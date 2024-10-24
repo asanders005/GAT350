d994f21669d1fa2371b05bafd6715fe8c19782a7
Aiden Sanders <aidensandersbiz@gmail.com>
2024-10-23 22:46:38 -0600

    Tweaks to finalize Rasterization & Added models.

--- SoftwareRenderer/Source/Main.cpp
+++ SoftwareRenderer/Source/Main.cpp
@@ -36,17 +36,7 @@ int main(int argc, char* argv[])
 	std::unique_ptr<Framebuffer> framebuffer = std::make_unique<Framebuffer>(*renderer.get(), renderer->GetWidth(), renderer->GetHeight());
 
 	Image scenery;
-	scenery.Load("Scenery.jpg");
-
-	Image eevee;
-	eevee.Load("Eevee.png");
-
-	Image pokeball;
-	pokeball.Load("image.png");
-
-	Image imageAlpha;
-	imageAlpha.Load("colors.png");
-	Post::Alpha(imageAlpha.m_buffer, 64);
+	scenery.Load("Shrine.jpg");
 
 	Color::SetBlendMode(BlendMode::NORMAL);
 
@@ -55,9 +45,9 @@ int main(int argc, char* argv[])
 	fox->Load("fox.obj");
 	fox->SetColor({ 255, 128, 0, 255 });
 	
-	/*std::shared_ptr<Model> gate = std::make_shared<Model>();
+	std::shared_ptr<Model> gate = std::make_shared<Model>();
 	gate->Load("gate.obj");
-	gate->SetColor({ 255, 128, 0, 255 });*/
+	gate->SetColor({ 255, 128, 0, 255 });
 	
 	std::shared_ptr<Model> tree = std::make_shared<Model>();
 	tree->Load("tree.obj");
@@ -65,17 +55,17 @@ int main(int argc, char* argv[])
 
 	std::vector<std::unique_ptr<Actor>> actors;
 
-	Transform transform{ {randomf(-10.0f, 10.0f), randomf(-10.0f, 10.0f), randomf(-10.0f, 10.0f)}, {0, randomf(-90, 90), 0}, glm::vec3{4}};
+	Transform transform{ { 0, 0, 0 }, { 0, -75, 0 }, glm::vec3{5}};
 	std::unique_ptr<Actor> actor = std::make_unique<Actor>(transform, fox);
 	actor->SetColor({ 255, 128, 0, 255 });
 	actors.push_back(std::move(actor));
 	
-	/*transform = { {randomf(-10.0f, 10.0f), randomf(-10.0f, 10.0f), randomf(-10.0f, 10.0f)}, {0, randomf(-90, 90), 0}, glm::vec3{4}};
+	transform = { { -20, 7, 25 }, { 0, -45, 0 }, glm::vec3{5}};
 	actor = std::make_unique<Actor>(transform, gate);
 	actor->SetColor({ 255, 28, 28, 255 });
-	actors.push_back(std::move(actor));*/
+	actors.push_back(std::move(actor));
 
-	transform = { {randomf(-10.0f, 10.0f), randomf(-10.0f, 10.0f), randomf(-10.0f, 10.0f)}, {0, randomf(-90, 90), 0}, glm::vec3{0.25f} };
+	transform = { { 25, -5, 40 }, {5, 75, 5}, glm::vec3{0.25f} };
 	actor = std::make_unique<Actor>(transform, tree);
 	actor->SetColor({ 255, 0, 255, 255 });
 	actors.push_back(std::move(actor));
@@ -106,66 +96,10 @@ int main(int argc, char* argv[])
 		int mx, my;
 		SDL_GetMouseState(&mx, &my);
 
-#pragma region Images
-		/*Color::SetBlendMode(BlendMode::NORMAL);
-		framebuffer->DrawImage(700, 450, scenery);*/
-		/*Color::SetBlendMode(BlendMode::ALPHA);
-		framebuffer->DrawImage(360, 250, eevee);*/
-		//Color::SetBlendMode(BlendMode::ADDITIVE);
-		//framebuffer->DrawImage(mx, my, pokeball);
-		//Color::SetBlendMode(BlendMode::MULTIPLY);
-		//framebuffer->DrawRect(10, 390, 200, 200, { 180, 25, 255, 128 });
-
-#pragma endregion
-
-#pragma region Primitives
-		//for (int i = 0; i < 3; i++)
-		//{
-		//	int x = rand() % 1000 - 100;
-		//	int y = rand() % 800 - 100;
-		//	/*int x2 = rand() % 400;
-		//	int y2 = rand() % 300;*/
-
-		//	//framebuffer->drawpoint(x, y, { 180, 0, 255, 255 });
-		//	//framebuffer->DrawLine(x, y, x2, y2, { 0, 255, 25, 255 });
-		//	framebuffer->DrawImage(x, y, pokeball);
-		//}	
-
-		//framebuffer->DrawLinearCurve(50, 50, 100, 100, { 255, 255, 255, 255 });
-		////framebuffer->DrawQuadraticCurve(100, 50, 200, 100, mx, my, 10, { 255, 255, 255, 255 });
-		//framebuffer->DrawCubicCurve(200, 100, 250, 200, mx, my, 300, 50, 50, { 255, 255, 255, 255 });
-
-		//int x, y;
-		//Math::CubicPoint(200, 100, 250, 200, mx, my, 300, 50, t, x, y);
-		//framebuffer->DrawRect(x - 20, y - 20, 40, 40, { 180, 0, 255, 255 });
-
-		/*framebuffer->DrawLine(-80, 5, 600, 40, { 0, 255, 25, 255 });
-		framebuffer->DrawLine(80, 150, 40, 275, { 0, 255, 25, 255 });
-		framebuffer->DrawRect(20, 20, 100, 100, { 0, 255, 180 });
-		framebuffer->DrawTriangle(200, 50, 50, 295, 375, 110, { 255, 120, 0 });
-		framebuffer->DrawCircle(225, 125, 50, { 0, 0, 255 });*/
-
-#pragma endregion
-
-#pragma region postProcess
-		//Post::Invert(framebuffer->m_buffer);
-		//Post::Monochrome(framebuffer->m_buffer);
-		//Post::Brightness(framebuffer->m_buffer, 40);
-		//Post::Noise(framebuffer->m_buffer, 100);
-		//Post::Threshold(framebuffer->m_buffer, 125);
-
-		//Post::BoxBlur(framebuffer->m_buffer, framebuffer->m_width, framebuffer->m_height);
-		//Post::Sharpen(framebuffer->m_buffer, framebuffer->m_width, framebuffer->m_height);
-		//Post::Edge(framebuffer->m_buffer, framebuffer->m_width, framebuffer->m_height, 120);
-		//Post::Emboss(framebuffer->m_buffer, framebuffer->m_width, framebuffer->m_height);
-		//Post::EmbossColor(framebuffer->m_buffer, framebuffer->m_width, framebuffer->m_height);
-		
-
-		/*Post::ColorBalance(framebuffer->m_buffer, 12, -7, 12);
-		Post::GaussianBlur(framebuffer->m_buffer, framebuffer->m_width, framebuffer->m_height);
-		Post::Posterize(framebuffer->m_buffer, 8);*/
-
-#pragma endregion
+		Color::SetBlendMode(BlendMode::NORMAL);
+		framebuffer->DrawImage(700, 450, scenery);
+		Post::ColorBalance(framebuffer->m_buffer, -20, -20, 10);
+		Post::Brightness(framebuffer->m_buffer, -80);
 		
 		if (input->GetMouseButtonDown(2))
 		{
@@ -179,13 +113,13 @@ int main(int argc, char* argv[])
 			if (input->GetKeyDown(SDL_SCANCODE_W)) direction.z += 1;
 			if (input->GetKeyDown(SDL_SCANCODE_S)) direction.z -= 1;
 
-			cameraTransform.rotation.y += input->GetMouseRelative().x * 0.5f;
-			cameraTransform.rotation.x += input->GetMouseRelative().y * 0.5f;
+			cameraTransform.rotation.y += input->GetMouseRelative().x * 0.1f;
+			cameraTransform.rotation.x += input->GetMouseRelative().y * 0.1f;
 			cameraTransform.rotation.x = Math::Clamp(cameraTransform.rotation.x, -89.99f, 89.99f);
 
 			glm::vec3 offset = cameraTransform.GetMatrix() * glm::vec4{ direction, 0 };
 
-			cameraTransform.position += offset * 50.0f * time->GetDeltaTime();
+			cameraTransform.position += offset * 15.0f * time->GetDeltaTime();
 		}
 		else
 		{
